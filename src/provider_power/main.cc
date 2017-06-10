2379dfeb3babbdf597b2c5ced54199a7f7d0ac83
olavoie <hp-elite-slim@sonia>
2017-06-10 17:25:10 -0400

    adding thread to poll ps

--- src/provider_power/main.cc
+++ src/provider_power/main.cc
@@ -1,15 +1,14 @@
 // ROS includes
 #include "provider_power_node.h"
+#include <thread>
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "provider_power_node");
     ros::NodeHandlePtr nh(new ros::NodeHandle("~"));
     provider_power::ProviderPowerNode ppn(nh);
-    ros::Rate loop_rate(1);
-    while (ros::ok()) {
-        ppn.PublishPowerData();
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    std::thread publishData(&provider_power::ProviderPowerNode::PollAllPs, ppn);
+    ros::spin();
+    publishData.join();
     return 0;
 }
