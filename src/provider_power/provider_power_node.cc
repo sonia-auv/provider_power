921d372d5980b2d5d98873ae1049e32b53629217
supertoto29 <francisalonzo29@gmail.com>
2021-03-17 00:44:34 +0000

    change rate

--- src/provider_power/provider_power_node.cc
+++ src/provider_power/provider_power_node.cc
@@ -70,7 +70,7 @@ namespace provider_power {
 // M E T H O D   S E C T I O N
 
     void ProviderPowerNode::Spin(){
-        ros::Rate r(5); // 10 hz
+        ros::Rate r(5); // 5 hz
 
         while(ros::ok())
         {
