5cd0a976a8ddc850032958204e9ccb563630b6ae
supertoto29 <francisalonzo29@gmail.com>
2021-07-20 00:56:52 +0000

    changed the cmd

--- src/provider_power/provider_power_node.h
+++ src/provider_power/provider_power_node.h
@@ -75,7 +75,7 @@ namespace provider_power {
 
         void pollCmd(uint8_t slave, uint8_t cmd);
 
-        uint8_t swapCmd[3] = {0,1,3}; // Voltage, current, motor read
+        uint8_t swapCmd[3] = {2,3,5}; // Voltage, current, motor read
         std::string voltageArray[10] = {"V_M1", "V_M2", "V_M3", "V_M4", "V_M5", "V_M6", "V_M7", "V_M8", "V_BAT1", "V_BAT2"};
         std::string currentArray[10] = {"C_M1", "C_M2", "C_M3", "C_M4", "C_M5", "C_M6", "C_M7", "C_M8", "C_BAT1", "C_BAT2"};
 
